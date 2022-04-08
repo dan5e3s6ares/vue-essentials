5c36072bdd5adb23487a7843af86a3401e6b7627
danielmeat <daniel.martins@team.me.at>
2022-04-08 15:24:48 -0400

    final code course

---
+++ src/components/Products.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>My Shop</h1>
+    <navbar :cart="cart" :cartQty="cartQty" :cartTotal="cartTotal"></navbar>
+    <price-slider
+      :sliderStatus="sliderStatus"
+      :maximum.sync="maximum"
+    ></price-slider>
+    <product-list :maximum="maximum" :products="products"></product-list>
+  </div>
+</template>
+<script>
+import Navbar from "./Navbar.vue";
+import PriceSlider from "./PriceSlider.vue";
+import ProductList from "./ProductList.vue";
+export default {
+  name: "products",
+  props: [
+    "cart",
+    "cartQty",
+    "cartTotal",
+    "products",
+    "maximum",
+    "sliderStatus",
+    "sliderState",
+  ],
+  components: {
+    Navbar,
+    PriceSlider,
+    ProductList,
+  },
+};
+</script>
